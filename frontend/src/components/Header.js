d633524b15d5c4860b0c9adbf63602be333dc728
E m <laceoneup@gmail.com>
2022-09-04 10:23:02 -0400

    first commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Header = () => {
+  return (
+    <Navbar bg="light" variant="light">
+      <Navbar.Brand href="/">Image Gallery</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;

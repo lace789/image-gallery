f8c690a52b71805161db5ead4270b7418d738755
E m <laceoneup@gmail.com>
2022-09-05 08:42:46 -0400

    Control Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
-import Search from "./components/Search";
+import Search from "./components/search";
 
-function App() {
+const App = () => {
+  const [word, setWord] = useState("");
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
+
   return (
     <div className="App">
       <Header title="Image Gallery" />
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { Col, Row, Container, Form, Button } from "react-bootstrap";
+import { Row, Col, Container, Form, Button } from "react-bootstrap";
 
-const Search = () => {
+const Search = ({ word, setWord, handleSubmit }) => {
   return (
     <Container className="mt-4">
-      <Row>
-        <Col></Col>
-        <Col>
-          <Form.Control type="search" placeholder="Search for image..." />
-        </Col>
-        <Col>
-          <Button variant="primary" type="submit">
-            Search
-          </Button>
-        </Col>
-      </Row>
+      <Form onSubmit={handleSubmit}>
+        <Row className={"justify-content-center"}>
+          <Col></Col>
+          <Col>
+            <Form.Control
+              type="text"
+              value={word}
+              onChange={(e) => setWord(e.target.value)}
+              placeholder="Search for image..."
+            />
+          </Col>
+          <Col>
+            <Button variant="primary" type="submit">
+              Search
+            </Button>
+          </Col>
+        </Row>
+      </Form>
     </Container>
   );
 };
